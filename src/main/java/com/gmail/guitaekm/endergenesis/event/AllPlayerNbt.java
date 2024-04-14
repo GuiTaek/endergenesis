7240da50e7bee394846a5d6f087f2c3cd949a80f
GuiTaek <109903402+GuiTaek@users.noreply.github.com>
2024-04-14 16:08:15 +0200

    fix repeatedly crashing after entering the pocket dimension

--- src/main/java/com/gmail/guitaekm/endergenesis/event/AllPlayerNbt.java
+++ src/main/java/com/gmail/guitaekm/endergenesis/event/AllPlayerNbt.java
@@ -1,36 +1,36 @@
 package com.gmail.guitaekm.endergenesis.event;
 
-import com.mojang.authlib.GameProfile;
-import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtIo;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerLoginNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Pair;
 import net.minecraft.util.WorldSavePath;
-import org.apache.logging.log4j.core.jmx.Server;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class AllPlayerNbt {
     protected static Map<UUID, NbtCompound> savedPlayers(MinecraftServer server) {
-        List<UUID> players = Arrays.stream(
+        List<Pair<UUID, NbtCompound>> players = Arrays.stream(
                         Objects.requireNonNull(
                                 server.getSavePath(WorldSavePath.PLAYERDATA).toFile().listFiles()
                         )
-                ).map(File::getName)
-                .filter(filename -> !filename.endsWith("dat_old"))
-                .map(filename -> filename.substring(0, filename.length() - 4))
-                .map(UUID::fromString).toList();
-        return players.stream().map((UUID uuid) -> new Pair<>(uuid, new GameProfile(uuid, null)))
-                .map(pair ->
-                        new Pair<>(
-                                pair.getLeft(),
-                                server.getPlayerManager().loadPlayerData(new ServerPlayerEntity(server, server.getOverworld(), pair.getRight()))
+                ).map(file -> new Pair<>(file.getName(), file))
+                .filter(pair -> !pair.getLeft().endsWith("dat_old"))
+                .map(pair -> {
+                    try {
+                        return new Pair<>(pair.getLeft(), NbtIo.readCompressed(pair.getRight()));
+                    } catch (IOException e) {
+                        return null;
+                    }
+                }).filter(Objects::nonNull)
+                .map(pair -> new Pair<>(
+                        UUID.fromString(pair.getLeft().substring(0, pair.getLeft().length() - 4)),
+                        pair.getRight())
                         )
-                ).collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
+                .toList();
+        return players.stream().collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
     }
     protected static Map<UUID, NbtCompound> onlinePlayers(MinecraftServer server) {
         return server
