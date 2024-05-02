d5c53e46c74a92950ceeaecfb250e550c09aeef2
GuiTaekM <guitaekm@gmail.com>
2024-05-02 20:54:12 +0200

    fixes #10

--- src/main/java/com/gmail/guitaekm/endergenesis/event/AllPlayerNbt.java
+++ src/main/java/com/gmail/guitaekm/endergenesis/event/AllPlayerNbt.java
@@ -8,6 +8,8 @@ import net.minecraft.util.WorldSavePath;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class AllPlayerNbt {
@@ -17,19 +19,33 @@ public class AllPlayerNbt {
                                 server.getSavePath(WorldSavePath.PLAYERDATA).toFile().listFiles()
                         )
                 ).map(file -> new Pair<>(file.getName(), file))
-                .filter(pair -> !pair.getLeft().endsWith("dat_old"))
+                .filter(pair -> pair.getLeft().endsWith("dat"))
+                .map(pair -> {
+                    String uuidString = pair.getLeft().substring(0, pair.getLeft().length() - 4);
+                    // sanitize string if this is a file that otherwise crashes the game according
+                    // to #10
+                    Pattern regex = Pattern.compile("(([a-fA-F0-9]+-){4})[a-fA-F0-9]+(-[0-9]+)?");
+                    Matcher matcher = regex.matcher(uuidString);
+                    // weirdly, without the method .matches(), the group function doesn't work
+                    // probably good to have this check anyway
+                    if (!matcher.matches()) {
+                        return null;
+                    }
+                    if(matcher.group(3) != null) {
+                        return null;
+                    }
+                    return new Pair<>(
+                                    UUID.fromString(uuidString),
+                                    pair.getRight()
+                            );
+                }).filter(Objects::nonNull)
                 .map(pair -> {
                     try {
                         return new Pair<>(pair.getLeft(), NbtIo.readCompressed(pair.getRight()));
                     } catch (IOException e) {
                         return null;
                     }
-                }).filter(Objects::nonNull)
-                .map(pair -> new Pair<>(
-                        UUID.fromString(pair.getLeft().substring(0, pair.getLeft().length() - 4)),
-                        pair.getRight())
-                        )
-                .toList();
+                }).filter(Objects::nonNull).toList();
         return players.stream().collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
     }
     protected static Map<UUID, NbtCompound> onlinePlayers(MinecraftServer server) {
